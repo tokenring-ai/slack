3f2482eb2d305d34f561b47d6791f28c4e4a55e6
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:40 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,18 @@
+import TokenRingApp from "@tokenring-ai/app";
+import {TokenRingPlugin} from "@tokenring-ai/app";
+import packageJSON from './package.json' with {type: 'json'};
+import SlackBotService, {SlackServiceConfigSchema} from "./SlackService.ts";
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    const slackConfig = app.getConfigSlice("slack", SlackServiceConfigSchema.optional());
+
+    if (slackConfig) {
+      app.addServices(new SlackBotService(app, slackConfig));
+    }
+  },
+} as TokenRingPlugin;
